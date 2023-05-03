8cd619d1def82bda5d5ffa0d06d4867bffa67d0f
ibitter <67180901+ibitter@users.noreply.github.com>
2023-05-03 18:33:28 +0800

    Update index.vue

--- src/components/Links/index.vue
+++ src/components/Links/index.vue
@@ -46,32 +46,22 @@ let linksData = [
   {
     icon: Blog,
     name: "博客",
-    link: "https://blog.imsyy.top/",
+    link: "https://wp.ibit.cf/",
   },
   {
     icon: Cloud,
     name: "网盘",
-    link: "https://pan.imsyy.top/",
+    link: "https://pan.ibit.cf/",
   },
   {
     icon: CompactDisc,
-    name: "音乐",
-    link: "https://music.imsyy.top/",
-  },
-  {
-    icon: Compass,
-    name: "起始页",
-    link: "https://nav.imsyy.top/",
+    name: "图床",
+    link: "https://img.ibit.cf/",
   },
   {
     icon: Book,
-    name: "网址集",
-    link: "https://web.imsyy.top/",
-  },
-  {
-    icon: Fire,
-    name: "今日热榜",
-    link: "https://hot.imsyy.top/",
+    name: "在线PS",
+    link: "https://ps.ibit.cf/",
   },
 ];
 
