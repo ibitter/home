6ca0d26f80a18cf4bca30c292bb8aa3a056f50c2
ibitter <67180901+ibitter@users.noreply.github.com>
2023-05-04 21:21:31 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import dayjs from 'dayjs';
 import fetchJsonp from "fetch-jsonp";
 
 /**
@@ -55,27 +56,103 @@ export const getHitokoto = async () => {
  */
 
 // 获取Monitors数据
+export function formatNumber(value) {
+  return (Math.floor(value * 100) / 100).toString();
+}
+
+export function formatDuration(seconds) {
+  let s = parseInt(seconds);
+  let m = 0;
+  let h = 0;
+  if (s >= 60) {
+    m = parseInt(s / 60);
+    s = parseInt(s % 60);
+    if (m >= 60) {
+      h = parseInt(m / 60);
+      m = parseInt(m % 60);
+    }
+  }
+  let text = `${s} 秒`;
+  if (m > 0) text = `${m} 分 ${text}`;
+  if (h > 0) text = `${h} 小时 ${text}`;
+  return text;
+}
 export const GetMonitors = async (apikey) => {
+  const dates = [];
+  const today = dayjs(new Date().setHours(0, 0, 0, 0));
+  for (let d = 0; d < 60; d++) {
+    dates.push(today.subtract(d, 'day'));
+  }
+
+  const ranges = dates.map((date) => `${date.unix()}_${date.add(1, 'day').unix()}`);
+  const start = dates[dates.length - 1].unix();
+  const end = dates[0].add(1, 'day').unix();
+  ranges.push(`${start}_${end}`);
+
   const postdata = {
     api_key: apikey,
     format: 'json',
     logs: 1,
+    log_types: '1-2',
+    logs_start_date: start,
+    logs_end_date: end,
+    custom_uptime_ranges: ranges.join('-'),
   };
   const response = await axios.post('https://cors.status.org.cn/uptimerobot/v2/getMonitors', postdata, {
     timeout: 10000
   });
-  let status = '未知';
   if (response.data.stat !== 'ok') {
-    let status = '网络错误';
     throw response.data.error;
   };
-  if (response.data.monitors[0].status === 2) {
-    status = '正常';
-  };
-  if (response.data.monitors[0].status === 9) {
-    status = '离线';
-  };
-  return status;
+  return response.data.monitors.map((monitor) => {
+
+    const ranges = monitor.custom_uptime_ranges.split('-');
+    const average = formatNumber(ranges.pop());
+    const daily = [];
+    const map = [];
+    dates.forEach((date, index) => {
+      map[date.format('YYYYMMDD')] = index;
+      daily[index] = {
+        date: date,
+        uptime: formatNumber(ranges[index]),
+        down: {
+          times: 0,
+          duration: 0
+        },
+      }
+    });
+
+    const total = monitor.logs.reduce((total, log) => {
+      if (log.type === 1) {
+        const date = dayjs.unix(log.datetime).format('YYYYMMDD');
+        total.duration += log.duration;
+        total.times += 1;
+        daily[map[date]].down.duration += log.duration;
+        daily[map[date]].down.times += 1;
+      }
+      return total;
+    }, {
+      times: 0,
+      duration: 0
+    });
+
+    const result = {
+      id: monitor.id,
+      name: monitor.friendly_name,
+      url: monitor.url,
+      average: average,
+      daily: daily,
+      total: total,
+      status: '未知',
+    };
+    if (monitor.status === 2) {
+      result.status = '正常';
+    };
+    if (monitor.status === 9) {
+      result.status = '离线';
+    };
+    return result;
+  });
 };
 
 /**
