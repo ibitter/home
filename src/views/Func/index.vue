39a1fdd382addd09b7c2a70ba2cbd8fc1a69d3c0
ibitter <67180901+ibitter@users.noreply.github.com>
2023-05-04 14:07:44 +0800

    Update index.vue

--- src/views/Func/index.vue
+++ src/views/Func/index.vue
@@ -132,7 +132,7 @@ onBeforeUnmount(() => {
         width: 100%;
         text-overflow: ellipsis;
         overflow-x: hidden;
-        white-space: nowrap;
+        #white-space: nowrap;
       }
     }
   }
