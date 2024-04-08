af086899d13d79f87d3d0891ad3c366354968cba
ibitter <ibitter@qq.com>
2024-04-08 12:21:44 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,3 @@
-import axios from "axios";
-import dayjs from "dayjs";
-import fetchJsonp from "fetch-jsonp";
-
-/**
- * 音乐播放器
- */
-
-// 获取音乐播放列表
 export const getPlayerList = async (server, type, id) => {
   const res = await fetch(
     `${import.meta.env.VITE_SONG_API}?server=${server}&type=${type}&id=${id}`,
@@ -23,108 +14,19 @@ export const getPlayerList = async (server, type, id) => {
     ).replace("http://", "https://");
 
     return data.map((v, i) => ({
-      title: v.name || v.title,
+      name: v.name || v.title,
       artist: v.artist || v.author,
-      src: domain + jsonpData.req_0.data.midurlinfo[i].purl,
-      pic: v.pic,
+      url: domain + jsonpData.req_0.data.midurlinfo[i].purl,
+      cover: v.cover || v.pic,
       lrc: v.lrc,
     }));
   } else {
     return data.map((v) => ({
-      title: v.name || v.title,
+      name: v.name || v.title,
       artist: v.artist || v.author,
-      src: v.url,
-      pic: v.pic,
+      url: v.url,
+      cover: v.cover || v.pic,
       lrc: v.lrc,
     }));
   }
-};
-
-/**
- * 一言
- */
-
-// 获取一言数据
-export const getHitokoto = async () => {
-  const res = await fetch("https://v1.hitokoto.cn");
-  return await res.json();
-};
-
-/**
- * Monitors
- */
-
-// 获取Monitors数据
-export function formatNumber(value) {
-  return (Math.floor(value * 100) / 100).toString();
-}
-
-export function formatDuration(seconds) {
-  let s = parseInt(seconds);
-  let m = 0;
-  let h = 0;
-  if (s >= 60) {
-    m = parseInt(s / 60);
-    s = parseInt(s % 60);
-    if (m >= 60) {
-      h = parseInt(m / 60);
-      m = parseInt(m % 60);
-    }
-  }
-  let text = `${s} 秒`;
-  if (m > 0) text = `${m} 分 ${text}`;
-  if (h > 0) text = `${h} 小时 ${text}`;
-  return text;
-}
-export async function GetMonitors(apikey, CountDays) {
-
-  const dates = [];
-  const today = dayjs(new Date().setHours(0, 0, 0, 0));
-  for (let d = 0; d < CountDays; d++) {
-    dates.push(today.subtract(d, 'day'));
-  }
-
-  const ranges = dates.map((date) => `${date.unix()}_${date.add(1, 'day').unix()}`);
-  const start = dates[dates.length - 1].unix();
-  const end = dates[0].add(1, 'day').unix();
-  ranges.push(`${start}_${end}`);
-
-  const postdata = {
-    api_key: apikey,
-    format: 'json',
-    logs: 1,
-    log_types: '1-2',
-    logs_start_date: start,
-    logs_end_date: end,
-    custom_uptime_ranges: ranges.join('-'),
-  };
-
-  return await axios.post('https://cors.status.org.cn/uptimerobot/v2/getMonitors', postdata, {
-    timeout: 10000
-  });
-}
-
-/**
- * 天气
- */
-
-// 获取高德地理位置信息
-export const getAdcode = async (key) => {
-  const res = await fetch(`https://restapi.amap.com/v3/ip?key=${key}`);
-  return await res.json();
-};
-
-// 获取高德地理天气信息
-export const getWeather = async (key, city) => {
-  const res = await fetch(
-    `https://restapi.amap.com/v3/weather/weatherInfo?key=${key}&city=${city}`,
-  );
-  return await res.json();
-};
-
-// 获取教书先生天气 API
-// https://api.oioweb.cn/doc/weather/GetWeather
-export const getOtherWeather = async () => {
-  const res = await fetch("https://api.oioweb.cn/api/weather/GetWeather");
-  return await res.json();
 };
